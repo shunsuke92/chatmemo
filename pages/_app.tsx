4213e890ff16106d3da0b615839c96392f930d98
shunsuke <shun.8000.sf@gmail.com>
2022-08-17 17:39:09 +0900

    MUI setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,32 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import CssBaseline from '@mui/material/CssBaseline'
+import { ThemeProvider, createTheme,responsiveFontSizes } from '@mui/material/styles'
+import useMediaQuery from '@mui/material/useMediaQuery'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const isDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
+  const theme = responsiveFontSizes(createTheme({
+    palette: {
+      mode: isDarkMode ? 'dark' : 'light'
+    },
+    typography: {
+      fontFamily: [
+       'Roboto',
+       'Noto Sans JP', 
+       'Helvetica',
+       'Arial',
+       'sans-serif',
+     ].join(','),
+    }
+  }))
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
---
+++ pages/_document.tsx
@@ -0,0 +1,29 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta charSet="UTF-8" />
+        <link rel="icon" type="image/svg+xml" href="/src/favicon.svg" />
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;500;700&display=swap"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
