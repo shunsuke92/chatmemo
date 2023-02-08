bf1fcdc150732d0d09a568bd8c0503d88612c7b9
shunsuke <shun.8000.sf@gmail.com>
2023-02-08 19:15:02 +0900

    iOSでアドレスバー分の表示位置のズレが生じる事象の対応

--- components/TimelineWrapper.tsx
+++ components/TimelineWrapper.tsx
@@ -11,7 +11,7 @@ export const TimelineWrapper = (props: TimelineWrapperProps) => {
   const { children } = props;
 
   const setFillHeight = () => {
-    const bottomBarHeight = window.innerWidth < 600 ? 72 : 80;
+    const bottomBarHeight = window.innerWidth < 600 ? 72 + 56 : 80 + 64;
     const vh = (window.innerHeight - bottomBarHeight) * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
   };
@@ -38,7 +38,8 @@ export const TimelineWrapper = (props: TimelineWrapperProps) => {
     <Box
       sx={{
         width: '100%',
-        height: 'calc(var(--vh, 1vh) * 100)',
+        height: '100%',
+        minHeight: 'calc(var(--vh, 1vh) * 100)',
         display: 'flex',
         alignItems: 'flex-start',
         justifyContent: 'center',
