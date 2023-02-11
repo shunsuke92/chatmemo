1be5a56a27b0f0ac99d0f6e1aebcd14f8b7771cb
shunsuke <shun.8000.sf@gmail.com>
2023-02-11 14:40:15 +0900

    軽微な修正

--- components/CommonTextField.tsx
+++ components/CommonTextField.tsx
@@ -46,7 +46,7 @@ export const CommonTextField = (props: CommonTextFieldProps) => {
         size='small'
         fullWidth
         multiline
-        sx={{ width: '479px', maxWidth: '75vw' }}
+        sx={{ width: '479px', maxWidth: '65vw' }}
         onChange={handleChange}
       />
     </Stack>
