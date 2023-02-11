f639a9e8f2499243841eb54e9f07af03e16525d4
shunsuke <shun.8000.sf@gmail.com>
2023-02-11 14:53:38 +0900

    調査用のログを追加

--- components/Contents.tsx
+++ components/Contents.tsx
@@ -107,6 +107,8 @@ export const Contents = () => {
   const scrollToBottom = () => {
     const element = document.documentElement;
     const bottom = element.scrollHeight - (visualViewport?.height ?? element.clientHeight);
+    console.log('visualViewport?.height', visualViewport?.height);
+    console.log('window.innerHeight', window.innerHeight);
     window.scroll(0, bottom);
   };
 
