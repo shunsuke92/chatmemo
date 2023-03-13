3267307fe24a89db572974e098b05ed6b603dd70
shunsuke <shun.8000.sf@gmail.com>
2023-03-13 10:27:45 +0900

    軽微な修正

--- components/OfflineChip.tsx
+++ components/OfflineChip.tsx
@@ -1,5 +1,6 @@
 import { useRecoilValue } from 'recoil';
 
+import CircleIcon from '@mui/icons-material/Circle';
 import Chip from '@mui/material/Chip';
 
 import { isOnlineState } from '../states/isOnlineState';
@@ -14,7 +15,8 @@ export const OfflineChip = () => {
           label='オフライン'
           color='error'
           variant='outlined'
-          sx={{ fontWeight: 500, mr: 4, borderWidth: '2px' }}
+          icon={<CircleIcon sx={{ width: '15px' }} />}
+          sx={{ fontWeight: 500, mr: { xs: 2, sm: 4 }, borderWidth: '2px' }}
         />
       )}
     </>
